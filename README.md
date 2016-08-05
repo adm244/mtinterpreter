ad6090ebfb2ae2ab51a45aeee0bc6c71d60b2abe
Dmitry Egorov <adm2444@gmail.com>
2016-08-05 22:08:59 +0300

    original release

---
+++ README.md
@@ -0,0 +1,5 @@
+**mtinterpreter** is an interpreter of Turing Machine written in Pascal just for fun.
+
+It uses Double Linked-Lists data structure to implement the "infinite" tape.
+
+Feel free to use, copy, modify, share and do whatever you want with these files. It's **Public Domain**.
---
+++ code/mtlist.pas
@@ -0,0 +1,119 @@
+{WRITTEN BY ADM244
+ 10/20/2015}
+unit mtlist;
+interface
+  
+  type
+    ListPointer = ^ListItem;
+    ListType = char;
+    ListItem = record
+      value : ListType;
+      prev, next : ListPointer;
+    end;
+  {#type}
+  
+  procedure Create( var list : ListPointer );
+  procedure PrintList( const list : ListPointer );
+  
+  procedure AppendLeft ( var list : ListPointer; value : ListType );
+  procedure AppendRight( var list : ListPointer; value : ListType );
+  
+  function GetLeft  ( const list : ListPointer ) : ListPointer;
+  function GetRight ( const list : ListPointer ) : ListPointer;
+  function GetLength( const list : ListPointer ) : Integer;
+  
+implementation
+  
+  procedure Create( var list : ListPointer );
+  begin
+    New(list);
+    list := nil;
+  end;
+  
+  procedure PrintList( const list : ListPointer );
+  var
+    current : ListPointer;
+  begin
+    current := GetLeft(list);
+    while not (current = nil) do begin
+      Write(current^.value, ' ');
+      current := current^.next;
+    end;
+    WriteLn;
+  end;
+  
+  procedure AppendLeft( var list : ListPointer; value : ListType );
+  var
+    item, current : ListPointer;
+  begin
+    New(item);
+    
+    item^.value := value;
+    item^.prev := nil;
+    
+    if list = nil then begin
+      item^.next := nil;
+      list := item;
+    end else begin
+      current := GetLeft(list);
+      item^.next := current;
+      current^.prev := item;
+    end;
+  end; {#AppendLeft}
+  
+  procedure AppendRight( var list : ListPointer; value : ListType );
+  var
+    item, current : ListPointer;
+  begin
+    New(item);
+    
+    item^.value := value;
+    item^.next := nil;
+    
+    if list = nil then begin
+      item^.prev := nil;
+      list := item;
+    end else begin
+      current := GetRight(list);
+      item^.prev := current;
+      current^.next := item;
+    end;
+  end; {#AppendRight}
+  
+  function GetLeft( const list : ListPointer ) : ListPointer;
+  var
+    current : ListPointer;
+  begin
+    current := list;
+    while not (current^.prev = nil) do
+      current := current^.prev;
+    
+    GetLeft := current;
+  end; {#GetList}
+  
+  function GetRight( const list : ListPointer ) : ListPointer;
+  var
+    current : ListPointer;
+  begin
+    current := list;
+    while not (current^.next = nil) do
+      current := current^.next;
+    
+    GetRight := current;
+  end; {#GetRight}
+  
+  function GetLength ( const list : ListPointer ) : Integer;
+  var
+    length : Integer;
+    current : ListPointer;
+  begin
+    length := 0;
+    current := GetLeft(list);
+    while not (current = nil) do begin
+      length := length + 1;
+      current := current^.next;
+    end;
+    GetLength := length;
+  end; {#GetLength}
+  
+end.
